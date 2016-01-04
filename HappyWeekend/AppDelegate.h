760c4d0645b9f6ccbcc7fa691820fd53334ce2f1
张茫原-芒果iOS <zhangmangyuan@gmail.com>
2016-01-04 11:23:17 +0800

    first commit

---
+++ HappyWeekend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyWeekend
+//
+//  Created by 张茫原 on 16/1/4.
+//  Copyright © 2016年 芒果科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HappyWeekend/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HappyWeekend
+//
+//  Created by 张茫原 on 16/1/4.
+//  Copyright © 2016年 芒果科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

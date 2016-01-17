c13ccf689a594da0a35b7b0b77619fc3c26a1433
张茫原-芒果iOS <zhangmangyuan@gmail.com>
2016-01-17 22:28:57 +0800

    2016年1月15日总代码

---
+++ HappyWeekend/Classes/Login/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  HappyWeekend
+//
+//  Created by 张茫原 on 16/1/15.
+//  Copyright © 2016年 芒果科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
